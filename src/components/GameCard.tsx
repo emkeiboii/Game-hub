4a472778a732ef0db4abb9dc09e1c68783633d8e
Mark <marklajom.work@gmail.com>
2023-04-03 12:23:23 +0200

    Fix typo with PlatformIconList

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
-import PlatfromIconLIst from "./PlatfromIconLIst";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -14,7 +14,7 @@ const GameCard = ({ game }: Props) => {
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
         <HStack justifyContent="space-between" marginBottom={3}>
-          <PlatfromIconLIst
+          <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic}></CriticScore>
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -17,7 +17,7 @@ interface Props {
   platforms: Platform[];
 }
 
-const PlatfromIconLIst = ({ platforms }: Props) => {
+const PlatformIconList = ({ platforms }: Props) => {
   const iconMap: { [key: string]: IconType } = {
     pc: FaWindows,
     playstation: FaPlaystation,
@@ -33,10 +33,14 @@ const PlatfromIconLIst = ({ platforms }: Props) => {
   return (
     <HStack marginY={"10px"}>
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} color="gray.500"></Icon>
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug]}
+          color="gray.500"
+        ></Icon>
       ))}
     </HStack>
   );
 };
 
-export default PlatfromIconLIst;
+export default PlatformIconList;
